2a58c95e31e6740d210a0e373387e7c8727eb81f
JoiceLisboa <joicepitzer@hotmail.com>
2023-06-21 21:48:59 -0300

    tela livros

--- src/pages/Livro/index.js
+++ src/pages/Livro/index.js
@@ -12,15 +12,49 @@ import AxiosInstance from "../../api/AxiosInstance";
 import { DataContext } from "../../context/DataContext";
 import { Header } from "../../../global/Header";
 
-export function Livro() {
-  return (
-    <View style={styles.container}>
-      <StatusBar />
-      <Header />
-      <Text>Livro</Text>
-    </View>
-  );
+export function Livro({route}) {
+  const navigation = useNavigation();
+  const { dadosUsuario } = useContext(DataContext);
+  const [livro, setLivro] = useState(null);
+  const idLivro = route.params?.idLivro;
 }
+
+  async function getLivros() {
+    try {
+      const newLivro = await AxiosInstance.get(`/livros/${idLivros}`, {
+        headers: { Authorization: `Bearer ${dadosUsuario?.token}` },
+      });
+      setLivro(newLivro.data);
+    } catch (error) {
+      console.log(error);
+    }
+    useEffect(() => {
+      getLivros();
+      console.log(idEditora);
+    }, []);
+    }
+
+  return (
+    <ScrollView>
+      <StatusBar />
+      <View style={styles.container}>
+        <Header />  
+
+        <Image
+                  source={{ uri: `data:image/png;base64,${livro.imagem}` }}
+                  style={styles.livro}
+                />
+                 <Text>Livro</Text> 
+                 <TextTrackList Nome do livro></TextTrackList>
+                 <TextTrackList Editora></TextTrackList>
+  
+      
+        </View>
+    </ScrollView>
+  );
+  
+  
+
 
 const styles = StyleSheet.create({
   container: {
